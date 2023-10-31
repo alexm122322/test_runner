41be469660bd2fefa1d73431496104ce41f7bde0
oleksii-minaiev <oleksii.minaiev@postindustria.com>
2023-10-31 17:37:59 +0200

    Refactored events. Added docstrings to the methods.

--- src/test_runner/_test_runner/events/events.py
+++ src/test_runner/_test_runner/events/events.py
@@ -6,17 +6,37 @@ SESSION_END = 'session_end'
 
 
 class Events:
+    """Events implementation"""
+
     def __init__(self):
         self._callbacks: List[FunctionType] = []
 
     def add_callback(self, func: FunctionType):
+        """Add callback if it is a function or method.
+
+        Args:
+            func: The function which should add to _callbacks.
+        """
+
         if (isinstance(func, (FunctionType, MethodType))):
             self._callbacks.append(func)
 
     def remove_callback(self, func: FunctionType):
+        """Remove callback if it is a function or method.
+
+        Args:
+            func: The function which should remove from _callbacks.
+        """
+
         if (isinstance(func, (FunctionType, MethodType))):
             self._callbacks.remove(func)
 
     def fire_event(self, event: str):
+        """Trigger all callbacks with event.
+
+        Args:
+            event: Event that will be transmitted to the callbacks.
+        """
+
         for callback in self._callbacks:
             callback(event)
