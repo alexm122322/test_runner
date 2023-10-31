aad7e39fe44e6ec06569cd1f33ce7655d93628ab
oleksii-minaiev <oleksii.minaiev@postindustria.com>
2023-10-31 17:16:23 +0200

    Added test for session.

---
+++ tests/config/test_session.py
@@ -0,0 +1,112 @@
+from test_runner._test_runner.config.session import Session
+from test_runner._test_runner.events.events import Events, SESSION_END, SESSION_START
+
+
+def session_start1():
+    pass
+
+
+def session_start2():
+    pass
+
+
+def session_end1():
+    pass
+
+
+def session_end2():
+    pass
+
+
+def test_start_session_func_call(mocker):
+    """Test session called session start function."""
+
+    events = Events()
+    mocked_session_start = mocker.patch(
+        f'{__name__}.{session_start1.__name__}')
+
+    session = Session(events, [session_start1], [])
+    session.start()
+    mocked_session_start.assert_called_once()
+
+
+def test_start_session_funcs_call(mocker):
+    """Test session called session start functions."""
+
+    events = Events()
+    mocked_session_start1 = mocker.patch(
+        f'{__name__}.{session_start1.__name__}')
+    mocked_session_start2 = mocker.patch(
+        f'{__name__}.{session_start2.__name__}')
+
+    session = Session(events, [session_start1, session_start2], [])
+    session.start()
+    mocked_session_start1.assert_called_once()
+    mocked_session_start2.assert_called_once()
+
+
+def test_end_session_func_call(mocker):
+    """Test session called session start function."""
+
+    events = Events()
+    mocked_session_end = mocker.patch(
+        f'{__name__}.{session_end1.__name__}')
+
+    session = Session(events, [session_end1], [])
+    session.start()
+    mocked_session_end.assert_called_once()
+
+
+def test_end_session_funcs_call(mocker):
+    """Test session called session start functions."""
+
+    events = Events()
+    mocked_session_end1 = mocker.patch(
+        f'{__name__}.{session_end1.__name__}')
+    mocked_session_end2 = mocker.patch(
+        f'{__name__}.{session_end2.__name__}')
+
+    session = Session(events, [session_end1, session_end2], [])
+    session.start()
+    mocked_session_end1.assert_called_once()
+    mocked_session_end2.assert_called_once()
+
+
+def start_session_event():
+    pass
+
+
+def end_session_event():
+    pass
+
+
+def test_fire_start_event(mocker):
+    """Test session fire start event."""
+
+    events = Events()
+    mocker = mocker.patch(f'{__name__}.{start_session_event.__name__}')
+
+    def event_hendler(event):
+        if event == SESSION_START:
+            start_session_event()
+
+    events.add_callback(event_hendler)
+    session = Session(events)
+    session.start()
+    mocker.assert_called_once()
+
+
+def test_fire_end_event(mocker):
+    """Test session fire end event."""
+
+    events = Events()
+    mocker = mocker.patch(f'{__name__}.{end_session_event.__name__}')
+
+    def event_hendler(event):
+        if event == SESSION_END:
+            end_session_event()
+
+    events.add_callback(event_hendler)
+    session = Session(events)
+    session.end()
+    mocker.assert_called_once()
---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+from mock import Mock
+
+@pytest.fixture
+def find(mocker):
+    return Mock()
